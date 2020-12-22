72a37c9a720b659be150f9ee784d66bad2900a40
kostyakoskos <korotkevichkostya@gmail.com>
2020-12-23 00:54:06 +0300

    final

--- file1.c
+++ file1.c
@@ -1,79 +1,83 @@
 #include <iostream>
-//#include <vector>
-//#include <string.h>
-//#include <stdio.h>
-//#include <sys/types.h>
+#include <vector>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <string>
 #include <cstring>
-//#include <signal.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <wait.h>
 
 using namespace std;
 
 
 int main()
 {
-/*  vector<string> user_in;
-  vector<const char*> user_str;
-  char s[200];
-  fgets(s,sizeof(s),stdin);
-  
-  string temp;
-  for(int i = 0; i < sizeof(s); i++)
-  {
-   if(s[i] == ' ' || s[i] == '\n')
-   {
-     user_in.push_back(temp);
-     temp="";
-   }
-   else{ temp+=s[i];}
-  }
+	vector<string> user_in;
+	vector<const char*> user_str;
+	char s[200];
+	char *arg[32];
+	while(1==1)
+	{
+		fgets(s,sizeof(s),stdin);
+		string temp;
+		for(int i = 0; i < sizeof(s); i++)
+		{
+			if(s[i] == ' ' || s[i] == '\n')
+			{
+				user_in.push_back(temp);
+				temp="";
+			}
+			else
+			{
+				temp+=s[i];
+			}
+		}
 
 
-  for(int i = 0; i <  user_in.size(); i++)
-  {  
-  user_str.push_back(user_in[i].c_str());
-  }
+		for(int i = 0; i <  user_in.size(); i++)
+		{
+			user_str.push_back(user_in[i].c_str());
+		}
 
-  user_str.push_back(nullptr);
-//  cout << "user_push" << endl;
-  pid_t pid = getpid();
+		user_str.push_back(nullptr);
+		pid_t pid = getpid();
 
-  for(int i = 0; i < user_str.size(); i++)
-  {
-   if((user_in[i] == "exit") == 1)
-   {
-      //cout << user_in[i];
-      //cout << "Kill"<< endl;
-      kill(pid,SIGTERM);
-      break;
-   }
+		if(user_in[0] == "exit")
+		{
+			kill(pid,SIGTERM);
+		}
 
-   if((pid = fork()) == -1)
-   {
-     cout << "fork error" << endl;
-   }
-   else if(pid == 0)
-   {
-    // cout << "!else " << i << endl;
-*/
-  string a = "./example2";
- 
-   char *args1[]={(char*)a.c_str(),NULL};
-   // execv(user_str[i],reinterpret_cast<char**>(user_str.data(),NULL));
-//cout<<(char*)a.c_str();
-  execv(args1[0], args1);// reinterpret_cast<char**>(user_str.data(),NULL));
-  cout<<"EXEC"<<endl;
-      //cout<< "right exec" << endl;  
-// start proccess
-   /* if(execve(args[0],args)<0)
-     {
-       cout << "Exit" << endl;
-       exit(0);
-     } */
-  //  }
-//   }
-  return 0;
- }
+		if((pid = fork()) == -1)
+		{
+			cout << "fork error" << endl;
+		}
+		else if(pid == 0)
+		{
+			for(int j = 0; j < user_str.size(); j++)
+			{
+				arg[j] = (char*)user_str[j];
+			}
+			if(execvp(user_str[0],arg) == -1)
+			{
+				cout<<"Execve error"<<endl;
+			}
+			cout<<"Execve successfull"<<endl;
+		}
+		else
+		{
+			wait(NULL);
+			user_str.clear();
+			user_in.clear();
+			bzero(s,200);
+			for(int i = 0; i<32;i++)
+			{
+				arg[i] = NULL;
+			}
+		}
+	}
+	return 0;
+}
